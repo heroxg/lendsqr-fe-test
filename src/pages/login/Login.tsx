754780ecf85c2a857d835be231c770bdbe9ebb71
heroxg <henryuyigue@gmail.com>
2023-01-27 10:50:41 +0100

    implemented form validation

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,19 +1,42 @@
 import MainLogo from '../../../src/assets/images/logo.png';
 import LoginImage from '../../../src/assets/images/login-img.png';
-import InputLabel from "@material-ui/core/InputLabel";
-import Visibility from "@material-ui/icons/Visibility";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
-import Input from "@material-ui/core/Input";
+import { useForm, SubmitHandler } from "react-hook-form";
 import React, { useState } from 'react'
 import './Login.css';
 
+interface LoginInput {
+    email: string;
+    password: string;
+  }
+
 const Login = () => {
     const [passwordShown, setpasswordShown] = useState(false);
+    const { register, formState: { errors }, handleSubmit} = useForm<LoginInput>();
 
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                email: e.target.value,
+                password: e.target.value
+            }
+        })
+    }
+
+    //toggle show and hide password text
     const togglePassword = (event: React.MouseEvent) => {
         event.preventDefault()
         setpasswordShown(!passwordShown);
+    }
+
+    //handle form submit
+    const onSubmit: SubmitHandler<LoginInput> = formData => {
+        console.log(formData.email);
     }
 
   return (
@@ -27,10 +50,20 @@ const Login = () => {
                 <div className="login_right">
                     <h3>Welcome!</h3>
                     <p className='info'>Enter details to login.</p>
-                    <form>
-                        <input type="email" placeholder='email' />
-                        <div className='pass_wrap'>
-                            <input type={passwordShown ? "text" : "password"} placeholder="password" />
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className='form_wrap'>
+                            <input type="email" placeholder='Email' 
+                            {...register("email", { required: "Email address is required" })} 
+                            onChange={handleChange}
+                            />
+                            <span className='error'>{errors.email && <p>{errors.email?.message}</p>}</span>
+                        </div>
+                        <div className='form_wrap'>
+                            <input type={passwordShown ? "text" : "password"} placeholder="password" 
+                            {...register("password", { required: "Password is required" })}
+                            onChange={handleChange}
+                            />
+                            <span className='error'>{errors.password && <p>{errors.password?.message}</p>}</span>
                             <div className='show'>
                                 <p onClick={togglePassword} className="password_text">
                                  {passwordShown ? "hide" : "show"}
@@ -38,7 +71,9 @@ const Login = () => {
                             </div>
                         </div>
                         <p className='pass'>forgot password?</p>
-                        <input type="submit" value="log in" />
+                        <input type="submit" value="log in" 
+                        disabled={formData.email === "" && formData.password === ""}
+                        />
                     </form>
                 </div>
             </div>
